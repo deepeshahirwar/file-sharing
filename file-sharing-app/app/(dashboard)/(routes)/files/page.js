b361d69167d61402e5d02c2a98f357b6cd983b6d
deepeshahirwar <deepeshahirwar7024@gmail.com>
2024-10-09 10:32:18 +0530

    data saving in storage

--- file-sharing-app/app/(dashboard)/(routes)/files/page.js
+++ file-sharing-app/app/(dashboard)/(routes)/files/page.js
@@ -1,19 +1,22 @@
  "use client"
  import React from 'react' 
  import { useState } from 'react'
-import {  SignIn, UserButton} from '@clerk/nextjs'
-import { Upload } from 'lucide-react'
+
 import UploadForm from '../upload/_components/UploadForm';
-
-
+import { getFirestore } from "firebase/firestore";
+import { collection, setDoc, doc } from "firebase/firestore";
+import { useUser } from '@clerk/nextjs';
 import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { app } from '@/filrebaseConfig';
+import { generateRandomString } from '@/app/_utils/RandomString'
 
 function Files({uploadBtnClickHandler}) {   
-   const [progress, setProgress] = useState(0);
+   const [progress, setProgress] = useState(0); 
+   const {user} = useUser();
 
   //  for file uploading and geting downloadURL of the url
   const storage = getStorage(app);
+  const db = getFirestore(app);
    const uploadFile = (file) => {
     const metadata = {
       contentType: file.type
@@ -29,8 +32,25 @@ function Files({uploadBtnClickHandler}) {
         setProgress(progress);
         progress == 100 &&  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           console.log('File available at', downloadURL);
+          // saveFileInfo(file, downloadURL);
         });
-      }, );
+      }, ); 
+
+      // for saving user file data in firebase database 
+      // const saveFileInfo = async (file, fileUrl) => {
+      //   const docId = generateRandomString().toString();
+      //   await setDoc(doc(db, "uploadedFile", docId), {
+      //     fileName: file?.name,
+      //     fileSize: file?.size,
+      //     fileType: file?.type,
+      //     fileUrl: fileUrl,
+      //     userEmail: user?.primaryEmailAddress.emailAddress,
+      //     userName: user?.fullName,
+      //     password: "",
+      //     fileId: docId,
+      //     shortUrl: process.env.NEXT_PUBLIC_BASE_URL + docId,
+      //   });
+      // };
 
 
    }
